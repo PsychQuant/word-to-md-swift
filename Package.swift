903e3f4c63dcba477ee38d1fca7dfc9915c3316a
che cheng <kiki830621@gmail.com>
2026-03-15 17:15:17 +0800

    refactor: rename dep doc-converter-swift → common-converter-swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "WordToMDSwift", targets: ["WordToMDSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/PsychQuant/doc-converter-swift.git", from: "0.4.0"),
+        .package(url: "https://github.com/PsychQuant/common-converter-swift.git", from: "0.4.0"),
         .package(url: "https://github.com/PsychQuant/ooxml-swift.git", from: "0.5.1"),
         .package(url: "https://github.com/PsychQuant/markdown-swift.git", from: "0.1.0"),
     ],
@@ -16,7 +16,7 @@ let package = Package(
         .target(
             name: "WordToMDSwift",
             dependencies: [
-                .product(name: "CommonConverterSwift", package: "doc-converter-swift"),
+                .product(name: "CommonConverterSwift", package: "common-converter-swift"),
                 .product(name: "OOXMLSwift", package: "ooxml-swift"),
                 .product(name: "MarkdownSwift", package: "markdown-swift"),
             ]
